74e3c678df8965d568a29fa08bffe8aef1874338
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2022-12-24 21:21:00 +0900

    feat: routing

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes, HashRouter } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+})
 
 const App = () => {
   return (
-    <div>App</div>
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <div className='ui container'>
+          <Routes>
+            <Route path="/" element={<div>Hello App</div>} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </ApolloProvider>
   )
 }
 
