2af4696fa8674bd81c4f31a0d113b6946226583f
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-01-23 15:09:38 +0900

    add user type

---
+++ server/src/schema/types/user_type.ts
@@ -0,0 +1,11 @@
+import { GraphQLID, GraphQLObjectType, GraphQLString } from 'graphql'
+
+const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: () => ({
+    id: { type: GraphQLID },
+    email: { type: GraphQLString }
+  })
+})
+
+export default UserType
