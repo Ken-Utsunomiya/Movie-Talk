3a96eadcaf1d3ff839f12e57ae91c48a2fc0d336
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-01-27 10:26:41 +0900

    refactor app component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react'
-import { Route, Routes, HashRouter } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/client'
+import React, { ReactNode } from 'react'
 
-import client from '../api/client'
-import MovieList from './MovieList'
-import MovieDetail from './MovieDetail'
+type AppProps = {
+  children: ReactNode
+}
 
 
-const App = () => {
+const App = (props: AppProps) => {
   return (
-    <ApolloProvider client={client}>
-      <HashRouter>
-        <div className='ui container'>
-          <Routes>
-            <Route path='/' element={<div>HOME</div>} />
-            <Route path="/movies" element={<MovieList />} />
-            <Route path="/movies/:id" element={<MovieDetail />} />
-          </Routes>
-        </div>
-      </HashRouter>
-    </ApolloProvider>
+    <div>
+      { props.children }
+    </div>
   )
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
+import { Route, Routes, HashRouter } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/client'
 
 import App from './components/App'
+import client from './api/client'
+import MovieList from './components/MovieList'
+import MovieDetail from './components/MovieDetail'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<App children={<div>HOME</div>} />} />
+          <Route path='/movies' element={<App children={<MovieList />} />} />
+          <Route path="/movies/:id" element={<App children={<MovieDetail />} />} />
+        </Routes>
+      </HashRouter>
+    </ApolloProvider>
+  )
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 )
 
