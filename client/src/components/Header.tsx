486f4ca4803d91c08c68d6c6fafa0fd1192cbbd0
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-01-29 13:59:03 +0900

    elaborate header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,54 @@
+import { useMutation, useQuery } from '@apollo/client'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import FETCH_USER from '../queries/fetchCurrentUser'
+import LOGOUT from '../mutations/logout'
 
 const Header = () => {
+  const { data, loading, error } = useQuery(FETCH_USER)
+  const [logout] = useMutation(LOGOUT)
+  const navigate = useNavigate()
+
+  const onLogoutClick = () => {
+    logout({
+      refetchQueries: [{ query: FETCH_USER }],
+      awaitRefetchQueries: true
+    })
+    .then(() => navigate('/'))
+  }
+
+  const renderButtons = () => {
+    if (loading) { return <div /> }
+    if (error) { return <p>ERROR: {error.message}</p> }
+
+    const { user } = data
+    if (user) {
+      return (
+        <li>
+          <a onClick={onLogoutClick} href="/#" >Logout</a>
+        </li>
+      )
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      )
+    }
+  }
+
   return (
     <nav>
       <div className='nav-wrapper blue'>
-        <Link to="/" className='brand-logo left'>Home</Link>
+        <Link to="/" className='brand-logo left'>Movie Talk</Link>
         <ul className='right'>
-          <div></div>
+          { renderButtons() }
         </ul>
       </div>
     </nav>
