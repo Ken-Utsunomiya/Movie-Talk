eea6bf9b8dfd6d9a66adcd2887055319dd4da001
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-01-30 11:26:55 +0900

    fix Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from '@apollo/client'
-import * as React from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import AppBar from '@mui/material/AppBar'
@@ -7,8 +7,6 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 
 import FETCH_USER from '../queries/fetchCurrentUser'
 import LOGOUT from '../mutations/logout'
@@ -52,17 +50,8 @@ const Header = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" color="default">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Movie Talk
           </Typography>
