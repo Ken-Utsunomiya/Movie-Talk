69f1af2c1f6848838d88a3fca6db9aa18822f233
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-02-06 11:54:11 +0900

    update movie doc and model

--- server/src/interfaces/movie.ts
+++ server/src/interfaces/movie.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose'
+
 import { CommentDoc } from './comment'
+import { ReviewDoc } from './review'
 
 export interface MovieDoc extends mongoose.Document {
   movieId: number
   title: String
   comments: CommentDoc[]
+  reviews: ReviewDoc[]
 }
 
 export interface MovieModel extends mongoose.Model<MovieDoc> {
   findComments(id: String): CommentDoc[]
   addCommentToMovie(id: String, title: String, content: String): CommentDoc[]
+  addReviewToMovie(id: String, title: String, content: String): ReviewDoc[]
 }
