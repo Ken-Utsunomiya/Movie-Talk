afd99f55d2754e3f1e9026c1680d53c8a9aa8707
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-02-07 10:47:58 +0900

    add review query

---
+++ server/src/schema/queries/review.ts
@@ -0,0 +1,16 @@
+import { GraphQLID, GraphQLNonNull } from 'graphql'
+
+import ReviewType from '../types/review_type'
+import Review from '../../models/review'
+
+const comment = {
+  type: ReviewType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLID) }
+  },
+  resolve(_: any, { id }: { id: String }) {
+    return Review.findById(id)
+  }
+}
+
+export default comment
