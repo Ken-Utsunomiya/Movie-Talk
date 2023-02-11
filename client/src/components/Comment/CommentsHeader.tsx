a5469c6945b358ee9b86abcbbcc69a61c1c60759
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-02-11 11:02:27 +0900

    add comment field

--- client/src/components/Comment/CommentsHeader.tsx
+++ client/src/components/Comment/CommentsHeader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { TextField } from '@mui/material'
 
 const CommentsHeader = ({ comment_count }: { comment_count: number }) => {
+  const [comment, setComment] = useState("")
   return (
     <div>
       <h4>{ comment_count } Comments</h4>
+      <TextField
+        id="standard-basic"
+        label="Add a comment ..."
+        variant="standard"
+        sx={{ display: "flex", maxWidth: 700 }}
+        margin="normal"
+        multiline
+        onChange={e => setComment(e.target.value)}
+      />
     </div>
   )
 }
