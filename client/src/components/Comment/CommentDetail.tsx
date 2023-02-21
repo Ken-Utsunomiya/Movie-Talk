026f4a4cc6ce0f79bee8a349116391d9ba23c4ed
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-02-21 22:15:48 +0900

    refresh comment page

--- client/src/components/Comment/CommentDetail.tsx
+++ client/src/components/Comment/CommentDetail.tsx
@@ -20,42 +20,7 @@ const CommentDetail = () => {
   }
 
   return (
-    <Box sx={{ marginTop: '2rem'}}>
-      <Paper
-        sx={{
-          p: 2,
-          margin: 'auto',
-          flexGrow: 1,
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Grid>
-                  <Typography gutterBottom variant="subtitle1" component="div">
-                    { data.comment.title }
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Posted by { data.comment.uid } at { data.comment.createdAt }
-                  </Typography>
-                </Grid>
-                <Typography variant="body2" gutterBottom>
-                  { data.comment.content }
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                  Remove
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Box>
+    <h1>Comment</h1>
   )
 }
 
